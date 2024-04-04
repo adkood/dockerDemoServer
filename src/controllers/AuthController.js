68e864c1f31b176e030b85e132cb70e2036b11f0
adkood <ashutoshnegi195@gmail.com>
2024-04-04 18:19:17 +0530

    cookie configuration changed a bit

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -23,7 +23,8 @@ const createSendToken = (user, statusCode, res) => {
 
     let cookieOption = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: true
+        httpOnly: true,
+        sameSite: 'None'
     }
 
     if (process.env.NODE_ENV === 'production') {
